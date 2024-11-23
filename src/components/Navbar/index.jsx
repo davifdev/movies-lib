c2ead6bf4a938635acc50f113d5373cf65d08fe8
Davi Fernandes <davi.fer159@gmail.com>
2024-11-23 11:24:51 -0300

    finish

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,9 +1,25 @@
-import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
 
 import "./styles.css";
 
 const Navbar = () => {
+  const [search, setSearch] = useState("");
+  const inputRef = useRef();
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!search) return;
+
+    navigate(`/search?q=${search}`);
+    setSearch("");
+    inputRef.current.focus();
+  };
+
   return (
     <>
       <nav id="navbar">
@@ -13,8 +29,14 @@ const Navbar = () => {
             MoviesLib
           </Link>
         </h2>
-        <form>
-          <input type="text" placeholder="Busque um filme" />
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Busque um filme"
+            onChange={(e) => setSearch(e.target.value)}
+            value={search}
+            ref={inputRef}
+          />
           <button type="submit">
             <BiSearchAlt2 />
           </button>
